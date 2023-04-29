02b1e6237a2767ed0f9f8ae7bba6452d24d26620
Ivan Krylov <kis.226@yandex.ru>
2023-04-29 10:23:56 +0300

    add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import logo from './images/logo.svg';
+import Header from './Header.js';
 
 function App() {
   return (
     <>
-    <header className="header">
-      <img src={logo} alt="Логотип: Место" className="header__logo" />
-    </header>
+    <Header />
 
     <main className="content">
 
