2bb5881e94785f1fdf7080e992061bd6ea25182a
Ivan Krylov <kis.226@yandex.ru>
2023-04-30 17:29:36 +0300

    move popups and handlers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,47 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
+import PopupNewPlace from './PopupNewPlace';
+import PopupProfileForm from './PopupProfileForm';
+import PopupNewAvatar from './PopupNewAvatar';
+import PopupDeleteConfirmation from './PopupDeleteConfirmation';
+import PopupPlacePhoto from './PopupPlacePhoto';
+
 export default function App() {
+
+  function handleEditAvatarClick() {
+    const popup = document.querySelector('.popup_type_new-avatar');
+    popup.classList.add('popup_opened');
+    console.log('handleEditAvatarClick');
+  };
+
+  function handleEditProfileClick() {
+    const popup = document.querySelector('.popup_type_profile-form');
+    popup.classList.add('popup_opened');
+    console.log('handleEditProfileClick');
+  };
+
+  function handleAddPlaceClick() {
+    const popup = document.querySelector('.popup_type_new-place');
+    popup.classList.add('popup_opened');
+    console.log('handleAddPlaceClick');
+  };
+
   return (
     <>
       <Header />
-      <Main />
+      <Main
+        onEditProfile={handleEditProfileClick}
+        isAddPlacePopupOpen={handleAddPlaceClick}
+        isEditAvatarPopupOpen={handleEditAvatarClick}
+      />
       <Footer />
+
+      <PopupProfileForm />
+      <PopupNewPlace />
+      <PopupNewAvatar />
+      <PopupDeleteConfirmation />
+      <PopupPlacePhoto />
 
       <template id="element-template">
         <div className="element">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,55 +1,27 @@
-import PopupNewPlace from './PopupNewPlace';
-import PopupProfileForm from './PopupProfileForm';
-import PopupNewAvatar from './PopupNewAvatar';
-import PopupDeleteConfirmation from './PopupDeleteConfirmation';
-import PopupPlacePhoto from './PopupPlacePhoto';
-
-export default function Main() {
-  function handleEditAvatarClick() {
-    const popup = document.querySelector('.popup_type_new-avatar');
-    popup.classList.add('popup_opened');
-    console.log('handleEditAvatarClick');
-  };
-
-  function handleEditProfileClick() {
-    const popup = document.querySelector('.popup_type_profile-form');
-    popup.classList.add('popup_opened');
-    console.log('handleEditProfileClick');
-  };
-
-  function handleAddPlaceClick() {
-    const popup = document.querySelector('.popup_type_new-place');
-    popup.classList.add('popup_opened');
-    console.log('handleAddPlaceClick');
-  };
+export default function Main(props) {
+  const { onEditProfile, isAddPlacePopupOpen, isEditAvatarPopupOpen } = props;
 
   return (
     <>
       <main className="content">
 
         <section className="profile">
-          <button className="profile__edit-avatar-button" type="button" aria-label="Редактировать Аватар" onClick={handleEditAvatarClick}>
+          <button className="profile__edit-avatar-button" type="button" aria-label="Редактировать Аватар" onClick={isEditAvatarPopupOpen}>
             <img alt="Аватар" className="profile__avatar" />
           </button>
 
           <div className="profile__info">
             <div className="profile__title">
               <h1 className="profile__name"></h1>
-              <button className="button profile__edit-button" type="button" aria-label="Редактировать профиль" onClick={handleEditProfileClick}></button>
+              <button className="button profile__edit-button" type="button" aria-label="Редактировать профиль" onClick={onEditProfile}></button>
             </div>
             <p className="profile__subtitle"></p>
           </div>
 
-          <button className="button profile__add-button" type="button" aria-label="Добавить место" onClick={handleAddPlaceClick}></button>
+          <button className="button profile__add-button" type="button" aria-label="Добавить место" onClick={isAddPlacePopupOpen}></button>
         </section>
 
         <section className="elements"></section>
-
-        <PopupProfileForm />
-        <PopupNewPlace />
-        <PopupNewAvatar />
-        <PopupDeleteConfirmation />
-        <PopupPlacePhoto />
 
       </main>
     </>
