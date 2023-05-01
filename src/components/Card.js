724cfc9f08b6472f3baaa28e2bbce02a8aeb6d10
Ivan Krylov <kis.226@yandex.ru>
2023-05-01 14:47:43 +0300

    fix Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,18 +5,16 @@ export default function Card({ card, onCardClick }) {
   const handleClick = () => { onCardClick(link, name); }
 
   return (
-    <>
-      <div className="element">
-        <button className="button element__button element__trash" type="button" aria-label="Удалить место" />
-        <img src={link} alt="Фото" className="element__image" onClick={handleClick} />
-        <div className="element__container">
-          <p className="element__title">{name}</p>
-          <div className="element__like-container">
-            <button className="button element__button element__like" type="button" aria-label="Поставить лайк" />
-            <span className="element__counter">{counter}</span>
-          </div>
+    <div className="element">
+      <button className="button element__button element__trash" type="button" aria-label="Удалить место" />
+      <img src={link} alt={`Фото: ${name}`} className="element__image" onClick={handleClick} />
+      <div className="element__container">
+        <p className="element__title">{name}</p>
+        <div className="element__like-container">
+          <button className="button element__button element__like" type="button" aria-label="Поставить лайк" />
+          <span className="element__counter">{counter}</span>
         </div>
       </div>
-    </>
+    </div>
   );
 }
