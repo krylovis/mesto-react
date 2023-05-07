a6838378d453ca945407f35da1695496293e4643
Ivan Krylov <kis.226@yandex.ru>
2023-05-07 09:14:39 +0300

    add isOwn and isLiked

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,13 @@
+import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 export default function Card({ card, onCardClick }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const likeButtonClassName = (
+    `button element__button element__like ${isLiked && 'element__like_active'}`
+  );
   const { link, name, likes } = card;
   const counter = likes.length;
 
@@ -6,12 +15,12 @@ export default function Card({ card, onCardClick }) {
 
   return (
     <div className="element">
-      <button className="button element__button element__trash" type="button" aria-label="Удалить место" />
+      {isOwn && <button className="button element__button element__trash" type="button" aria-label="Удалить место" />}
       <img src={link} alt={`Фото: ${name}`} className="element__image" onClick={handleClick} />
       <div className="element__container">
         <p className="element__title">{name}</p>
         <div className="element__like-container">
-          <button className="button element__button element__like" type="button" aria-label="Поставить лайк" />
+          <button className={likeButtonClassName} type="button" aria-label="Поставить лайк" />
           <span className="element__counter">{counter}</span>
         </div>
       </div>
