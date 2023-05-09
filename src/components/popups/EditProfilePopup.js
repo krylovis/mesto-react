d269dfece66570a89ce06c63893ef86a24ae4521
Ivan Krylov <kis.226@yandex.ru>
2023-05-09 10:40:38 +0300

    add useForm hook for EditProfilePopup

--- src/components/popups/EditProfilePopup.js
+++ src/components/popups/EditProfilePopup.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import PopupWithForm from './PopupWithForm';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import { useForm } from '../../hooks/useForm';
 
 export default function EditProfilePopup(props) {
   const currentUser = React.useContext(CurrentUserContext);
   const { isOpen, onClose, onUpdateUser, isLoading } = props;
-
-  const [userInfo, setUserInfo] = React.useState({ name: '', about: '' });
+  const { values, handleChange, setValues } = useForm({ name: '', about: '' });
 
   React.useEffect(() => {
-    setUserInfo((state) => ({
-      ...state,
-      ...currentUser,
-    }));
-  }, [currentUser, isOpen]);
+    setValues((state) => ({ ...state, ...currentUser, }));
+  }, [currentUser, setValues, isOpen]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    onUpdateUser(userInfo);
-  };
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setUserInfo(state => ({
-      ...state,
-      [name]: value,
-    }));
+    onUpdateUser(values);
   };
 
   return (
@@ -41,11 +30,11 @@ export default function EditProfilePopup(props) {
       inactiveButton={false}
     >
       <label className="popup__label" htmlFor="inputName">
-        <input className="popup__input" id="inputName" type="text" name="name" value={userInfo.name} onChange={handleChange} placeholder="ФИО" required minLength="2" maxLength="40" />
+        <input className="popup__input" id="inputName" type="text" name="name" value={values.name} onChange={handleChange} placeholder="ФИО" required minLength="2" maxLength="40" />
         <span className="popup__input-error inputName-error"></span>
       </label>
       <label className="popup__label" htmlFor="inputJob">
-        <input className="popup__input" id="inputJob" type="text" name="about" value={userInfo.about} onChange={handleChange} placeholder="Профессия" required minLength="2" maxLength="200" />
+        <input className="popup__input" id="inputJob" type="text" name="about" value={values.about} onChange={handleChange} placeholder="Профессия" required minLength="2" maxLength="200" />
         <span className="popup__input-error inputJob-error"></span>
       </label>
     </PopupWithForm>
