45cc0ca15e6af89cc0b9a19357d27912ba057f33
Krylov Ivan <krylovis@mchs.ru>
2023-05-22 12:20:15 +0300

    add header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <header className="header">
-      <img src={logo} alt="Логотип: Место" className="header__logo" />
-    </header>
+      <Link to="/">
+        <img src={logo} alt="Логотип: Место" className="header__logo" />
+      </Link>
+
+      <div className="header__links-container">
+        {location.pathname === "/sign-up" && <Link className="link header__link" to="/sign-in">Войти</Link>}
+        {location.pathname === "/sign-in" && <Link className="link header__link" to="/sign-up">Регистрация</Link>}
+        {location.pathname === "/" && <span className="header__login">email@mail.com</span>}
+        {location.pathname === "/" && <Link className="link header__link header__link_out" to="/sign-in">Выйти</Link>}
+      </div>
+    </header >
   );
 }
