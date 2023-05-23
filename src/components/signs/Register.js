24ff8d7c9fe5929e898d6df1b9dcf158f845672f
Krylov Ivan <krylovis@mchs.ru>
2023-05-23 08:27:15 +0300

    add register

--- src/components/signs/Register.js
+++ src/components/signs/Register.js
@@ -1,9 +1,20 @@
 import SignWithForm from './SignWithForm';
+import { useForm } from '../../hooks/useForm';
+import { register } from '../../utils/Auth';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+  const { values, handleChange } = useForm({ email: '', password: '' });
+  const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log('Register');
+    register(values)
+      .then((data) => {
+        console.log('data', data);
+        navigate('/', { replace: true });
+      })
+      .catch(err => console.log(err));
   };
 
   return (
@@ -16,12 +27,12 @@ export default function Register() {
       isLink={true}
     >
       <label className="sign__label" htmlFor="inputRegisterEmail">
-        <input className="sign__input" id="inputRegisterEmail" type="email" name="register-email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}" placeholder="Email" required minLength="2" maxLength="40" />
+        <input className="sign__input" id="inputRegisterEmail" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}" value={values.email} onChange={handleChange} placeholder="Email" required minLength="2" maxLength="40" />
         <span className="sign__input-error inputRegisterEmail-error"></span>
       </label>
 
       <label className="sign__label" htmlFor="inputRegisterPassword">
-        <input className="sign__input" id="inputRegisterPassword" type="password" name="register-password" placeholder="Пароль" required minLength="6" maxLength="200" />
+        <input className="sign__input" id="inputRegisterPassword" type="password" name="password" value={values.password} onChange={handleChange} placeholder="Пароль" required minLength="6" maxLength="200" />
         <span className="sign__input-error inputRegisterPassword-error"></span>
       </label>
     </SignWithForm>
--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,15 +1,14 @@
-const BASE_URL = 'https://api.nomoreparties.co';
+const BASE_URL = 'https://auth.nomoreparties.co';
 
 const headers = {
-  'Accept': 'application/json',
   'Content-Type': 'application/json'
 };
 
-const defaultOptions = (email, password) => {
+const defaultOptions = (password, email) => {
   return {
     method: 'POST',
     headers: headers,
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ password, email }),
   }
 };
 
@@ -22,19 +21,18 @@ const getResponse = (res) => {
   return Promise.reject(`Ошибка: ${res.status}`);
 };
 
-export const register = (email, password) => {
-  return request(`${BASE_URL}/signup`, defaultOptions(email, password))
+export const register = ({ password, email }) => {
+  return request(`${BASE_URL}/signup`, defaultOptions(password, email))
 };
 
-export const authorize = (email, password) => {
-  return request(`${BASE_URL}/signin`, defaultOptions(email, password))
+export const authorize = ({ password, email }) => {
+  return request(`${BASE_URL}/signin`, defaultOptions(password, email))
 };
 
 export const tokenVerification = (token) => {
   return request(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
     }
