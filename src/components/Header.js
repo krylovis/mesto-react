23e1b2c271068b52880dfaa5dcdb95b5f2cf65d5
Krylov Ivan <krylovis@mchs.ru>
2023-05-24 12:06:43 +0300

    add menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 export default function Header(props) {
   const { userEmail } = props;
+
+  const [isOpen, setIsOpen] = React.useState(false);
   const location = useLocation();
+
+  const toggleMenu = () => setIsOpen(isOpen => !isOpen);
+  const menuButtonClassName = `button header__menu-button ${isOpen && 'header__menu-button_close'}`;
 
   return (
     <header className="header">
@@ -17,6 +23,8 @@ export default function Header(props) {
         {location.pathname === "/" && <span className="header__login">{userEmail}</span>}
         {location.pathname === "/" && <Link className="link header__link header__link_out" to="/sign-in">Выйти</Link>}
       </div>
+
+      {/* <button className={menuButtonClassName} type="button" title={isOpen ? 'Закрыть меню' : 'Открыть меню'} aria-label="Кнопка меню" onClick={toggleMenu} /> */}
     </header >
   );
 }
